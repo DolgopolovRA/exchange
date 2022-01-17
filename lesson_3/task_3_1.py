0fb9da533ee3d6216da17b8bb77485b8bc36855e
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-01-17 17:35:34 +0300

    dz_3

---
+++ lesson_3/task_3_1.py
@@ -0,0 +1,22 @@
+
+
+def num_translate_adv(num_d):
+    list_translate = {'one': 'один',
+                      'two': 'два',
+                      'three': 'три',
+                      'four': 'четыре',
+                      'five': 'пять',
+                      'six': 'шесть',
+                      'seven': 'семь',
+                      'eight': 'восемь',
+                      'nine': 'девять',
+                      'ten': 'десять'}
+    if num_d.istitle():
+        print(list_translate.get(num_d.lower()).capitalize())
+    else:
+        print(list_translate.get(num_d))
+
+
+num = input('Введите слово для перевода: ')
+
+num_translate_adv(num)
---
+++ lesson_3/task_3_3.py
@@ -0,0 +1,35 @@
+
+def thesaurus(*args):
+    dict_f = {}
+
+    for i in args:
+        sp = dict_f.get(i[0], False)
+        if sp:
+            dict_f.update({i[0]: sp + [i]})
+        else:
+            dict_f.update({i[0]: [i]})
+
+    return dict_f
+
+
+def thesaurus_adv(*args):
+    dict_f = {}
+    dict_in = {}
+
+    for i in args:
+        key = i[i.find(" ")+1]  # получаем ключ первой буквы фамилии
+        dict_1 = dict_f.get(key, False)
+        if dict_1:  # если получили словарь
+            sp_in = dict_1.get(i[0], False)  # проверяем наличие ключа по первой букве имени
+            if sp_in:  # если есть список
+                dict_1.update({i[0]: sp_in + [i]}) # обновляем список
+            else:
+                dict_1.update({i[0]: [i]}) # создаём новый словарь
+        else: # нужно создать словарь
+            dict_f.update({key: {i[0]: [i]}})
+
+    return dict_f
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Алексей"))
+print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
