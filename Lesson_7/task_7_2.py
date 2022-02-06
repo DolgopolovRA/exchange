a1280665d366f0100ee22116268d13349fc68f5b
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-02-06 11:25:51 +0300

    1

--- Lesson_7/task_7_2.py
+++ Lesson_7/task_7_2.py
@@ -2,7 +2,6 @@ import yaml
 import os
 
 
-<<<<<<< Updated upstream
 def create_struct(data, way):
     if isinstance(data, dict):
         for key, val in data.items():
@@ -25,32 +24,3 @@ with open('config.yaml', 'r', encoding='utf-8') as r:
     content = yaml.load(r, Loader=yaml.FullLoader)
 start_way, _ = os.path.split(os.path.abspath('task_7_2.py'))
 create_struct(content, start_way)
-=======
-def dict_r(dict_f):
-    out_key = ''
-    for key, val in dict_f.items():
-        if isinstance(val, dict):
-            print('вызов')
-            out_key = out_key + val
-            dict_r(val)
-        else:
-            out_val = val
-    print(out_key)
-    print(out_val)
-
-
-with open('config.yaml', 'r', encoding='utf-8') as rf:
-    content = yaml.load(rf, Loader=yaml.FullLoader)
-# dict.
-result = dict_r(content)
-print(result)
-# if isinstance(content, dict):
-#     for key, val in content.items():
-#         if not os.path.exists(key):
-#             os.mkdir(key)
-#         if isinstance(val, dict):
-#             for key_1, val_1 in val.items():
-#                 if not os.path.exists(key_1):
-#                     os.mkdir(key_1)
-#                 if isinstance(val, dict):
->>>>>>> Stashed changes
