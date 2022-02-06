934c96b4b6d97e49622135e9ce1a2755b2b7d6a8
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-02-06 11:13:11 +0300

    1

--- Lesson_7/task_7_2.py
+++ Lesson_7/task_7_2.py
@@ -8,11 +8,14 @@ def create_struct(data, way):
             if not os.path.exists(os.path.join(way, key)):
                 os.mkdir(os.path.join(way, key))
             way = os.path.join(way, key)
-            create_struct(val, way)
+            way = create_struct(val, way)
     else:
         for elem in data:
-            with open(os.path.join(way, elem), 'a', encoding='utf-8'):
-                pass
+            if isinstance(elem, dict):
+                create_struct(elem, way)
+            else:
+                with open(os.path.join(way, elem), 'a', encoding='utf-8'):
+                    pass
         way, _ = os.path.split(way)
         return way
 
@@ -20,6 +23,4 @@ def create_struct(data, way):
 with open('config.yaml', 'r', encoding='utf-8') as r:
     content = yaml.load(r, Loader=yaml.FullLoader)
 start_way, _ = os.path.split(os.path.abspath('task_7_2.py'))
-
 create_struct(content, start_way)
-
--- Lesson_7/task_7_5.py
+++ Lesson_7/task_7_5.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 root_dir, _ = os.path.split(os.path.abspath('task_7_4.py'))
 dict_res = {}
@@ -8,9 +9,20 @@ for root, dirs, files in os.walk(root_dir):
         size = os.stat(os.path.join(root, file)).st_size
         key = f'1'.ljust(len(str(size))+1, '0')
         val = dict_res.get(key)
-        ext = file.split('.')[-1]
-        if val is not None:
-            dict_res[key] = str(int(val[0]) + 1)
+        if len(file.split('.')) > 1:
+            ext = file.split('.')[-1]
         else:
-            dict_res.setdefault(key, ['1', [].append(ext)])
+            ext = ''
+        if val is not None:
+            if val[1].count(ext) == 0:
+                val[1].append(ext)
+            dict_res[key] = [str(int(val[0]) + 1), val[1]]
+        else:
+            dict_res.setdefault(key, ['1', [ext]])
 print(dict_res)
+for k, v in dict_res.items():
+    dict_res.update({k: tuple(v)})
+print(dict_res)
+_, way = os.path.split(root_dir)
+with open(way + '_summary.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(dict_res))
