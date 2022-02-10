d4e07d47c644e950410fdadee470ebc000edaef7
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-02-10 19:25:35 +0300

    1

---
+++ Lesson_9/task_9_5.py
@@ -0,0 +1,30 @@
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+
+class Pen(Stationery):
+    def __init__(self, title):
+        super.__init__(title)
+
+    def draw(self):
+        print(f'Запуск отрисоки ручкой {self.title}')
+
+
+class Pencil(Stationery):
+    def __init__(self, title):
+        super.__init__(title)
+
+    def draw(self):
+        print(f'Запуск отрисоки карандашом {self.title}')
+
+
+class Handle(Stationery):
+    def __init__(self, title):
+        super.__init__(title)
+
+    def draw(self):
+        print(f'Запуск отрисоки маркером {self.title}')
