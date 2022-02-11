8de78db15c165fbf4034574ce8a6355f23540af7
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-02-11 13:46:47 +0300

    1

--- Lesson_9/task_9_1.py
+++ Lesson_9/task_9_1.py
@@ -1,6 +1,7 @@
 import time
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
+import threading
 
 
 class TrafficLight(QMainWindow):
@@ -11,107 +12,34 @@ class TrafficLight(QMainWindow):
         self.but = QtWidgets.QPushButton(self)
         self.but.move(150, 120)
         self._color = 'grey'
+        self.stop = 1
         self.but.setStyleSheet(f'background-color: {self._color};')
-        self.but.clicked.connect(self.running)
+        self.but.clicked.connect(self.stop_circle)
+
+    def stop_circle(self):
+        self.stop = 0
 
     def running(self):
-        # while True:
-        self._color = 'red'
-        self.but.setStyleSheet(f'background-color: {self._color};')
-        print('red')
-        time.sleep(7)
-        self._color = 'yellow'
-        self.but.setStyleSheet(f'background-color: {self._color};')
-        print('yellow')
-        time.sleep(2)
-        self._color = 'green'
-        self.but.setStyleSheet(f'background-color: {self._color};')
-        print('green')
-        time.sleep(10)
-        self._color = 'yellow'
-        self.but.setStyleSheet(f'background-color: {self._color};')
-        print('yellow')
-        time.sleep(2)
+        while self.stop:
+            self._color = 'red'
+            self.but.setStyleSheet(f'background-color: {self._color};')
+            time.sleep(7)
+            self._color = 'yellow'
+            self.but.setStyleSheet(f'background-color: {self._color};')
+            time.sleep(2)
+            self._color = 'green'
+            self.but.setStyleSheet(f'background-color: {self._color};')
+            time.sleep(5)
+            self._color = 'yellow'
+            self.but.setStyleSheet(f'background-color: {self._color};')
+            time.sleep(2)
 
 
 app = QApplication([])
 window = TrafficLight()
-# window.running()
+t = threading.Thread(target=window.running, args=())
+t.start()
 window.show()
 app.exec_()
-# window.running()
-
-
-
-# from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
-# from PyQt5.QtGui import QPalette
-# from PyQt5.QtCore import Qt
-# import time
-# import PyQt5
-#
-#
-# class TrafficLight(PyQt5):
-#     def __init__(self):
-#         super().__init__(self)
-#         # self.__color = None
-#
-#         self.__color = None
-#
-#     def rotation(self):
-#         app = QApplication([])
-#         app.setStyle('Fusion')
-#         window = QWidget()
-#         layout = QVBoxLayout()
-#         layout.addWidget(QPushButton())
-#         pallet = QPalette()
-#         # while True:
-#         pallet.setColor(QPalette.Button, Qt.red)
-#         app.setPalette(pallet)
-#         # time.sleep()
-#         window.setLayout(layout)
-#         window.show()
-#         app.exec_()
-#         # while True:
-#         # if self.__color is None:
-#         #     self.__color = Qt.red
-#         #     return self.__color, 7
-#         # elif self.__color == Qt.red:
-#         #     self.__color = Qt.yellow
-#         #     return self.__color, 2
-#         # # elif self.__color == Qt.yellow
-#
-#         # self.__color = Qt.red
-#             # print('красный')
-#             # pallet.setColor(QPalette.Button, self.__color)
-#         # return self.__color
-#             # time.sleep(7)
-#             # self.__color = Qt.yellow
-#             # print('жёлтый')
-#             # return self.__color
-#             # time.sleep(2)
-#             # self.__color = Qt.green
-#             # print('зелёный')
-#             # return self.__color
-#             # time.sleep(5)
-#             # self.__color = Qt.yellow
-#             # print('жёлтый')
-#             # return self.__color
-#             # time.sleep(2)
-#
-#
-# traffic_light = TrafficLight()
-# print(traffic_light.rotation())
-#
-# # app = QApplication([])
-# # app.setStyle('Fusion')
-# # window = QWidget()
-# # layout = QVBoxLayout()
-# # layout.addWidget(QPushButton())
-# # pallet = QPalette()
-# # pallet.setColor(QPalette.Button, Qt.red)
-# # app.setPalette(pallet)
-# # window.setLayout(layout)
-# # window.show()
-# # app.exec_()
-#
-#
+window.stop_circle()
+t.join()
