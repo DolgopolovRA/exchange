9cdf39e21ed0b8ed38f2eb34d467eaff46da27de
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-08-21 23:48:04 +0300

    dz_3_alg

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -21,4 +21,51 @@ f1dcaeeafeb855965535d77c55782349444b
 или, если вы уже знаете, как Python взаимодействует с базами данных,
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
+
+
+from hashlib import pbkdf2_hmac
+import psycopg2
+from os import urandom
+
+
+def ret_psw_hash(password, salt):
+    psw_hash = pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 1000)
+    return psw_hash.hex()
+
+
+_salt = urandom(32)
+print(_salt.hex())
+psw = input('Введите пароль: ')
+try:
+    connect_db = psycopg2.connect(user='postgres', password='root', host='127.0.0.1', port='5432', database='testdb')
+    cursor = connect_db.cursor()
+    cursor.execute('INSERT INTO psw(user, hash) VALUES (%s, %s)', ('user_1', ret_psw_hash(psw, _salt)))
+    connect_db.commit()
+    cursor.close()
+    connect_db.close()
+
+except Exception as err:
+    print("Ошибка при работе с БД PostgreSQL", err)
 """
+
+import json
+from hashlib import pbkdf2_hmac
+from os import urandom
+
+
+def ret_psw_hash(password, salt):
+    psw_hash = pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 1000)
+    return psw_hash.hex()
+
+
+_salt = urandom(32)
+psw_1 = input('Введите пароль: ')
+with open('psw.json', 'w') as psw_file:
+    json.dump(ret_psw_hash(psw_1, _salt), psw_file)
+
+psw_2 = input('Введите пароль ещё раз: ')
+with open('psw.json', 'r') as psw_file:
+    if json.load(psw_file) == ret_psw_hash(psw_2, _salt):
+        print('Вы ввели правильный пароль')
+    else:
+        print('Вы ввели не правильный пароль')
