cf918f721efcd4626175fd6d40737a75b7c4411f
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-09-27 16:58:38 +0300

    1

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-# ввод значения N (эту переменную не менять)
-N = int(input())
-
-# здесь продолжайте программу
-def get_sum(n):
-    for i in range(n):
-        yield sum(range(1, i+1))
-
-
-a = get_sum(N)
-for _ in range(25):
-    print(next(a))
-
+lst = input().split().sort(key=lambda x: x.index(tp))
+print(*lst)
